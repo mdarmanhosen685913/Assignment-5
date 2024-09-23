89a5a3373896a1e1f8ec2e641d85120b9efd4573
mdarmanhosen685913 <armanfarazi01@gmail.com>
2024-09-24 03:24:56 +0600

    home-to-blog-replace

--- btn.js
+++ btn.js
@@ -1,6 +1,22 @@
+// btn using replace
 document.getElementById('donation').addEventListener('click', function(){
     btn('donationBtn')
+    
 })
 document.getElementById('history').addEventListener('click', function(){
     btn('historyBtn')
+})
+
+// btn color change
+document.getElementById('donation').addEventListener('click', function(){
+    color('donation')
+    
+})
+document.getElementById('history').addEventListener('click', function(){
+    color('history')
+})
+
+// blog to home replace
+document.getElementById('blog').addEventListener('click', function(){
+    window.location.href = "homes.html";
 })
--- style.js
+++ style.js
@@ -1,5 +1,12 @@
+// button using replace
 function btn (id){
     document.getElementById('donationBtn').classList.add('hidden');
     document.getElementById('historyBtn').classList.add('hidden');
     document.getElementById(id).classList.remove('hidden');
+}
+// btn color change
+function color (colorId){
+    document.getElementById('donation').classList.remove('bg-[#B4F461]')
+    document.getElementById('history').classList.remove('bg-[#B4F461]')
+    document.getElementById(colorId).classList.add('bg-[#B4F461]')
 }
