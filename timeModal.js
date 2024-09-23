a3852299a6be6dfd01b1f1a930fc061bef652b91
mdarmanhosen685913 <armanfarazi01@gmail.com>
2024-09-24 04:37:37 +0600

    'responsive

--- timeModal.js
+++ timeModal.js
@@ -1,18 +1,18 @@
 function timeDate1(){
     const date = new Date();
     const time = date.toLocaleString();
-
+    my_modal_1.showModal();
     document.getElementById('timeNow1').innerText = time ;
 }
 function timeDate2(){
     const date = new Date();
     const time = date.toLocaleString();
-
+    my_modal_1.showModal();
     document.getElementById('timeNow2').innerText = time ;
 }
 function timeDate3(){
     const date = new Date();
     const time = date.toLocaleString();
-
+    my_modal_1.showModal();
     document.getElementById('timeNow3').innerText = time ;
 }
